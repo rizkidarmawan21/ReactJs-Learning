d7f8448861b4a5db79f2f0ed0b351dcb589bd263
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2021-11-23 06:28:05 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  const userLogged = "Angga BWA";
-  const userLoggedJSX = <strong>BWA for future! </strong>
 
 
   return (
-    <div className="App">
-      {userLogged}
-      {userLoggedJSX}
+    <div className="Parentbox">
+        <Fotoproduk/>
+        <ProdukInfo category="SPORT" title ="SEPATU LARANG" price="100000"/>
     </div>
+  );
+}
+
+function Fotoproduk(){
+  return (
+    <div className="Foto">
+      <img src="sepatu2.jpg" alt="" />
+    </div>
+  );
+}
+
+
+
+function ProdukInfo({category,title,price}) {
+  return (
+        <div className="Deskripsi">
+          <p className="Cate">{category}</p>
+          <h1 className="Title"> {title}</h1>
+          <p className="Price">IDR {price}</p>
+          <p className="Info">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fugit unde mollitia, molestias vel inventore a nam tempora corrupti dolores nobis obcaecati repudiandae sequi earum cupiditate blanditiis voluptatem alias deserunt totam dolorem sint sit? Cum beatae atque iusto quas? Dolores aspernatur quibusdam fuga laboriosam accusamus eaque harum libero, laudantium deleniti!
+          </p>
+        </div>
   );
 }
 
