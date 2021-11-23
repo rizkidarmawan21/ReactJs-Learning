5eb17e515874c5d89a103f57164ce0c175706caa
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2021-11-23 07:00:44 +0700

    add Discount product

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
 
     <div className="Parentbox">
         <Fotoproduk/>
-        <ProdukInfo category="SPORT" title ="SEPATU LARANG" price="100000"/>
+        <ProdukInfo isDiscount="yes" category="SPORT" title ="SEPATU LARANG" price="100000"/>
+
     </div>
   );
 }
@@ -22,13 +23,30 @@ function Fotoproduk(){
 }
 
 
+function CheckDiscount(props){
+  const {isDiscount} = props;
+  if (isDiscount == "yes") {
+    return (
+      <p>Diskon 50% Off</p>
+    );
+  }else if ( isDiscount == "comming"){
+    return (
+      <p>Akan ada diskon</p>
+    );
+  }else {
+    return (
+      <p>Belum Ada Discount</p>
+    );
+  }
+}
 
-function ProdukInfo({category,title,price}) {
+function ProdukInfo({category,title,price,isDiscount}) {
   return (
         <div className="Deskripsi">
           <p className="Cate">{category}</p>
           <h1 className="Title"> {title}</h1>
           <p className="Price">IDR {price}</p>
+          <CheckDiscount isDiscount={isDiscount} />
           <p className="Info">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, fugit unde mollitia, molestias vel inventore a nam tempora corrupti dolores nobis obcaecati repudiandae sequi earum cupiditate blanditiis voluptatem alias deserunt totam dolorem sint sit? Cum beatae atque iusto quas? Dolores aspernatur quibusdam fuga laboriosam accusamus eaque harum libero, laudantium deleniti!
           </p>
